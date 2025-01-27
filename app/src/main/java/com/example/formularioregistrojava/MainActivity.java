be053d68688b8ca5b1b3a53ee8a0c0ad343d15ed
angelramos <angelgonzalez171104@gmail.com>
2025-01-27 08:15:08 -0600

    Cambios en agregar el toast

--- app/src/main/java/com/example/formularioregistrojava/MainActivity.java
+++ app/src/main/java/com/example/formularioregistrojava/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.formularioregistrojava;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtNombre;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        txtNombre = findViewById(R.id.editTextText2);
+        btnEnviar = findViewById(R.id.button);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre = txtNombre.getText().toString();
+                Toast.makeText(MainActivity.this, "Nombre: " + nombre, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
